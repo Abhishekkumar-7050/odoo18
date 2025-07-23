2f176f579c716d4bff5fa489cde7e145871eb043
Abhishek Kumar <abhishekofficial7050@gmail.com>
2025-07-23 17:57:23 +0530

    dashbord piechart graph chart sms intigration

--- addons/appoinment/models/twilio_config.py
+++ addons/appoinment/models/twilio_config.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+import requests
 
 class TwilioConfig(models.Model):
     _name = 'twilio.config'
@@ -10,3 +11,32 @@ class TwilioConfig(models.Model):
     auth_token = fields.Char(string="Auth Token", required=True)
     twilio_number = fields.Char(string="Twilio Number", required=True)
     active = fields.Boolean(default=True)
+
+
+
+
+    def action_test_connection(self):
+       
+        for rec in self:
+            url = f"https://api.twilio.com/2010-04-01/Accounts/{rec.account_sid}.json"
+            try:
+                response = requests.get(url, auth=(rec.account_sid, rec.auth_token))
+                if response.status_code == 200:
+                    message = "Twilio connection successful!"
+                else:
+                    message = f"Failed to connect to Twilio Invalid credentials!"
+            except Exception as e:
+                message = f"Connection error: {str(e)}"
+
+            # Raise notification
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Twilio Connection Test',
+                    'message': message,
+                    'type': 'success' if response.status_code == 200 else 'danger',
+                    'sticky': False,
+                }
+            }
+
